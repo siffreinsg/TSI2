6a73c34b7c37a6819d221c0ea928f1083e85dd29
Siffrein Sigy <hello@siffreinsigy.me>
2021-01-23 17:36:58 +0100

    DM

--- Projet 6/projet.py
+++ Projet 6/projet.py
@@ -1,10 +1,9 @@
 from os import chdir
 from math import sin, sqrt, cos, asin, pi
-from time import strftime
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
-chdir(r"C:\Users\Siffrein JACOBE\Documents\Coding\TSI2_info\Projet 6")
+# chdir(r"C:\Users\Siffrein JACOBE\Documents\Coding\TSI2_info\Projet 6")
+chdir(r"D:\Documents\Coding\TSI2\Projet 6")
 
 fid = open("OuCest.kml", "r")
 file = fid.readlines()
@@ -44,6 +43,7 @@ def Affiche_GPS_n(n):
     i = j = 0
     while j < n and i < len(file):
         if is_word_in_string("<when>", file[i]):
+            print(file[i], "\n", file[i+1])
             i += 1  # On saute deux lignes
             j += 1  # On a affiché un couple supplémentaire
         i += 1
@@ -52,7 +52,6 @@ def Affiche_GPS_n(n):
 # Question 5
 def heure(ligne):
     if "when" in ligne:
-        # <when>2014-01-05T09:41:07.580Z</when>
         heure = ligne[ligne.find("T") + 1: ligne.find("Z")]
         h = heure.split(":")
         return [float(h[0]), float(h[1]), float(h[2])]
@@ -92,46 +91,82 @@ def orthodromie(loA, laA, loB, laB):
 
 
 def distance(T):
-    return [orthodromie(T[p][4], T[p][5], T[p+1][4], T[p+1][5]) for p in range(len(T) - 1)]
+    return [orthodromie(T[p][3], T[p][4], T[p+1][3], T[p+1][4]) for p in range(len(T) - 1)]
 
 
 # Question 9
-def distance_total(D):
-    return sum(D)
+def distance_total(distances):
+    return sum(distances)
 
 
 # Question 10
 def denivele(T):
     total = 0
-    for p in range(len(T) - 1):
-        if T[p+1][5] > T[p][5]:
-            total += T[p + 1][5] - T[p][5]
-    return total / 1000
+    for p in range(1, len(T)):
+        if T[p][5] > T[p-1][5]:
+            total += T[p][5] - T[p-1][5]
+    return total
 
 
 # Question 11
 def distance_cumulee(T):
-    D = distance(T)
-    return [distance_total(D), denivele(T)]
+    distances = distance(T)
+    distances_cumulees = []
+
+    for i, d in enumerate(distances):
+        denivele = T[i+1][5] - T[i][5]
+        distances_cumulees += [((d * 1000)**2 + denivele**2)**(1/2)]
+
+    return distances_cumulees
 
 
 # Question 12
+def vitesse(T):
+    distances = distance(T)
+    vitesses = []
+    for p in range(1, len(T)):
+        delta_d = distances[p-1]
+        delta_t = (T[p][0] + T[p][1] / 60 + T[p][2] / 3600) - (T[p-1][0] + T[p-1][1] / 60 + T[p-1][2] / 3600)
+        vitesses.append(delta_d / delta_t)
+    return vitesses
+
+
 T = tableau_donnees(file)
+distances = distance(T)
+distances_cumulees = distance_cumulee(T)
 
-distances_entre_points = distance(T)
-distance_total_parcourue, denivele_positif = distance_cumulee(T)
+x_axis = [0]
+altitudes = [T[0][5]]
+vitesses = [0.0] + vitesse(T)
+
+for k in range(1, len(T)):
+    x_axis += [x_axis[k-1] + distances[k-1]]
+    altitudes += [T[k][5]]
 
 
-vitesses = [0.0]
+fig, ax1 = plt.subplots()
+ax1.set_xlabel("Distance (km)")
+ax1.set_ylabel("Altitude (km)", color="red")
+ax1.plot(x_axis, altitudes, color="red")
+ax1.tick_params(axis='y', labelcolor="red")
 
-for p in len(1, len(T)):
-    delta_t = (T[p][0] + T[p][1] / 60 + T[p][2] / 3600) - (T[p - 1][0] + T[p - 1][1] / 60 + T[p - 1][2] / 3600)
-    delta_d = distances_entre_points[p - 1] // 1000
-    vitesses += [delta_d / delta_t]
+ax2 = ax1.twinx()
+ax2.set_ylabel("Vitesse (km/h)", color="blue")
+ax2.plot(x_axis, vitesses, color="blue")
+ax2.tick_params(axis="y", labelcolor="blue")
 
 
-# abscisses, ordonnees = [], []
-# for position in T[1:]:
-#     # position = [heures, minutes, secondes, longitude, latitude, altitude]
-#     abscisses += [position[0] * 3600 + position[1] * 60 + position[2]]
-#     ordonnees += []
+print("Distance totale:", distance_total(distances))
+print("Dénivelé positif:", denivele(T))
+
+fig.suptitle("Profil du parcours", fontsize=16)
+plt.show()
+
+
+# Question 13
+def vitesse_moy(T):
+    vitesses = vitesse(T)
+    return sum(vitesses) / len(vitesses)
+
+
+print("Vitesse moyenne:", vitesse_moy(T))
